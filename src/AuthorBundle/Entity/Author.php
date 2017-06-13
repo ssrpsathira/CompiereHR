ecf4e272890ea0fa78ed03da18191e78617109d0
ssrpsathira <ssrpsathira@gmail.com>
2017-06-14 00:54:49 +1200

    * add getters and setter for Article and Author entities

--- src/AuthorBundle/Entity/Author.php
+++ src/AuthorBundle/Entity/Author.php
@@ -2,8 +2,85 @@
 
 namespace AuthorBundle\Entity;
 
+use ArticleBundle\Entity\Article;
+use Doctrine\Common\Collections\ArrayCollection;
+
 class Author
 {
     private $id;
     private $name;
+    private $articles;
+
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Author
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Add article
+     *
+     * @param Article $article
+     *
+     * @return Author
+     */
+    public function addArticle(Article $article)
+    {
+        $this->articles[] = $article;
+
+        return $this;
+    }
+
+    /**
+     * Remove article
+     *
+     * @param Article $article
+     */
+    public function removeArticle(Article $article)
+    {
+        $this->articles->removeElement($article);
+    }
+
+    /**
+     * Get articles
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getArticles()
+    {
+        return $this->articles;
+    }
 }
