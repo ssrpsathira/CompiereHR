e8cfb7afca0bbd70db7b828802906b97c7cd863e
ssrpsathira <ssrpsathira@gmail.com>
2017-06-13 21:05:12 +1200

    * add databse config

---
+++ src/AuthorBundle/AuthorBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AuthorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AuthorBundle extends Bundle
+{
+}
---
+++ src/AuthorBundle/Controller/AuthorController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AuthorBundle\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: ssrp
+ * Date: 13/06/17
+ * Time: 8:05 PM
+ */
+class AuthorController
+{
+    public function listAction(){
+        
+    }
+}
