79535e6f41bd3391ad4d23dfd209c906afb3d812
Sathira <ssrpsathira@gmail.com>
2017-10-04 21:24:42 +1300

    * update install script only to add necessary steps of env creation

--- install.sh
+++ install.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # Install PHP
-sudo apt-get install python-software-properties software-properties-common
-sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
-sudo apt-get update
-sudo apt-get remove php5-common -y
-sudo apt-get purge php5-common -y
-sudo apt-get install php7.0 php7.0-fpm php7.0-mysql php7.0-xml -y
+# sudo apt-get install python-software-properties software-properties-common
+# sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
+# sudo apt-get update
+# sudo apt-get remove php5-common -y
+# sudo apt-get purge php5-common -y
+# sudo apt-get install php7.0 php7.0-fpm php7.0-mysql php7.0-xml -y
 
 # Install MySQL
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
-sudo apt-get -y install mysql-server
+# sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+# sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+# sudo apt-get -y install mysql-server
 
 # Setup app environment
 php bin/console server:start 0.0.0.0:8000
 php bin/console doctrine:database:create
-php bin/console doctrine:schema:update --force
+# php bin/console doctrine:schema:update --force
 php composer.phar install
 
 # Setup backup db location
-cd /opt
-sudo mkdir backups
-cd backups
-sudo mkdir database
-cd database
-sudo chmod 777 ./
+# cd /opt
+# sudo mkdir backups
+# cd backups
+# sudo mkdir database
+# cd database
+# sudo chmod 777 ./
