f0823f62f15d97a22075aa64d28e206b4662114e
Sathira <ssrpsathira@gmail.com>
2017-10-04 23:42:58 +1300

    * add leave controller

--- src/LeaveBundle/Controller/LeaveController.php
+++ src/LeaveBundle/Controller/LeaveController.php
@@ -3,8 +3,12 @@
 namespace LeaveBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Response;
 
 class LeaveController extends FOSRestController
 {
-
+    public function listAction()
+    {
+        return new Response('Working', Response::HTTP_OK);
+    }
 }
